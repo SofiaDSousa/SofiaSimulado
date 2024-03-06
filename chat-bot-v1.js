2006247bda6c854dcf353036feba7585aa47e253
SofiaDSousa <124785107+SofiaDSousa@users.noreply.github.com>
2024-03-06 20:11:56 -0300

    Update chat-bot-v1.js

--- chat-bot-v1.js
+++ chat-bot-v1.js
@@ -1,22 +1,23 @@
-import {jogosRecomendados, melhorJogo, jogoPreferido, ondeJogar, papoFurado, mostProbablyQuestion } from './can-i-answer.js'
+import { jogosRecomendados, melhorJogo, jogoPreferido, ondeJogar, papoFurado, mostProbablyQuestion } from './can-i-answer.js';
+
+//possivel perguntar a ser feita, separando a frase em uma array de palavras.
+let pergunta = "Qual é o melhor jogo?";
+pergunta = pergunta.toLowerCase();
+pergunta = pergunta.replace('?', ''); // tira o ? da frase. 
+pergunta = pergunta.split(' ');
 
 
+const bancos = [jogosRecomendados, melhorJogo, jogoPreferido, ondeJogar, papoFurado];
 
-let pergunta = "Qual é o melhor jogo?"
-pergunta = pergunta.toLowerCase()
-pergunta = pergunta.split(' ')
-
-
-const bancos = [
-    jogosRecomendados, melhorJogo, jogoPreferido, ondeJogar, papoFurado, 
-]
-
-for (let i = 0; i < pergunta.length; i++) {
-    for (let j = 0; j < bancos.length; j++) { 
-        if (bancos[j].probablyWords.indexOf(pergunta[i]) !== -1) {
-            bancos[j].matched++
+function compararPergunta() {
+    for (let i = 0; i < pergunta.length; i++) {
+        for (let j = 0; j < bancos.length; j++) {
+            if (bancos[j].probablyWords.indexOf(pergunta[i]) !== -1) {
+                bancos[j].matched++;
+            }
         }
     }
 }
 
-console.log(mostProbablyQuestion(pergunta))
+compararPergunta();
+mostProbablyQuestion(); 
